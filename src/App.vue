96263674f1030eda5c2da0d76eeede4c73399e9a
JuliaOliveira26 <julinhalves2006@gmail.com>
2023-04-03 16:56:05 -0300

    ex2

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 import { ref } from "vue";
 const name = ref(``)
 const idade = ref();
+const email = ref(``)
+let mostrardiv = ref(true)
+const nome2 = ref('')
+const idadeF2 = ref()
+const emailF2 = ref('')
 
 </script>
 
@@ -12,7 +17,7 @@ const idade = ref();
     <label>Nome:</label>
     <input type="text" v-model="name" />
 
-    <p>Nome: {{ name }}</p>
+  
     <button @click="name = ``">Limpar</button>
   </div>
   <br>
@@ -24,8 +29,56 @@ const idade = ref();
   <br>
   <div>
     <label> E-mail:</label>
-    <input type="text">
+    <input type="text" v-model="email" />
   </div>
+
+
+  <div class="Enviar">
+    <button @click="mostrardiv= !mostrardiv"> Enviar </button>
+    <div v-if = "mostrardiv">
+    <p>Nome:{{ name }}</p>
+    <p>Idade:{{ idade }}</p>
+      <p>E-mail:{{ email }}</p>
+</div>
+  </div>
+ <form @submit.prevent='formulario1'>
+  <h1>Formulário 2 </h1>
+  <br>
+  <div>
+    <label>Nome:</label>
+    <input type="text" v-model="nome2" minlength="3" maxlength="20" />
+
+  
+    <button @click="nome2= ``">Limpar</button>
+  </div>
+  <br>
+  <div>
+    <label>idade:</label>
+    <input type="number" v-model="idadeF2"  date id="datemax" name="datemax" max="60" />
+    <input type="date" id="datemin" name="datemin" min="18">"
+    <button @click="idadeF2 = ``">Limpar</button>
+  </div>
+  <br>
+  <div>
+    <label> E-mail:</label>
+    <input type="text" v-model="emailF2" />
+  </div>
+
+
+  <div class="Enviar">
+    <button @click="mostrardiv= !mostrardiv"> Enviar </button>
+    <div v-if = "mostrardiv">
+      <p>Os dados enviados foram:</p>
+    <p>Nome:{{ nome2 }}</p>
+    <p>Idade:{{ idadeF2 }}</p>
+      <p>E-mail:{{ emailF2}}</p>
+</div>
+  </div>
+
+
+
+</form>
+
 
 <br>
 
